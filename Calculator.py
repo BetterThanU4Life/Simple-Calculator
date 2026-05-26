bed493b03eff852751b8848590c23d98c57bab87
BetterThanU4Life <alyaanhsiddiqui@gmail.com>
2026-05-26 15:47:39 -0500

    Calculator.py
    Basically the whole thing

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+
+print('Welcome to the calculator.py!')
+
+print('Please Enter a Number')
+num1 = input()
+print('Please pick another number')
+num2 = input()
+
+SUM = int(num1) + int(num2)
+PRODUCT = int(num1) * int(num2)
+DIFFERENCE = int(num1) - int(num2)
+QUOTIENT = int(num1) / int(num2)
+
+print('The answer to your addition equation is...')
+
+print(SUM)
+print('The answer to your multiplication equation is...')
+print(PRODUCT)
+print('The answer to your subtraction problem is...')
+print(DIFFERENCE)
+print('The answer to your division problem is...')
+print(QUOTIENT)
+
+print('Thats a lot of math!')
+print('Would you like to do another equation?')
+
